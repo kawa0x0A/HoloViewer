27875bd3de5b92bce9127ccbda6a7f8a377a80b9
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2020-11-06 08:57:07 +0000

    フルスクリーン用の処理を追加

---
+++ HoloViewer/IWindowMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoloViewer
+{
+    public interface IWindowMode
+    {
+        void WindowMode ();
+
+        void FullScreen ();
+    }
+}
