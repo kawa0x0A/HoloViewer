7aeee47c071fbba4fe965e33aefc28a753a5c892
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2020-11-11 14:57:20 +0900

    アプリケーションのフィードバックに関する処理を追加

---
+++ HoloViewer.Windows/FeedBackWindow.xaml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HoloViewer.Windows
+{
+    /// <summary>
+    /// FeedBackWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class FeedBackWindow : Window
+    {
+        public string Version { get; set; } = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        private const int GWL_STYLE = -16;
+        private const int WS_MINIMIZEBOX = 0x00020000;
+        private const int WS_MAXIMIZEBOX = 0x00010000;
+
+        public FeedBackWindow ()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+        private static extern IntPtr GetWindowLongPtr (IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint="SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr (IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        private void Window_Loaded (object sender, RoutedEventArgs e)
+        {
+            var handle = (new WindowInteropHelper(this)).Handle;
+            var style = GetWindowLongPtr(handle, GWL_STYLE);
+            var styleValue = style.ToInt64();
+
+            styleValue &= (~(WS_MAXIMIZEBOX | WS_MINIMIZEBOX));
+
+            SetWindowLongPtr(handle, GWL_STYLE, new IntPtr(styleValue));
+        }
+
+        private void Hyperlink_RequestNavigate (object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+        }
+
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ HoloViewer/FeedBackInfo.cs
@@ -0,0 +1,11 @@
+namespace HoloViewer
+{
+    public static class FeedBackInfo
+    {
+        public const string RepositoryPageUrl = @"https://github.com/kawa0x0A/HoloViewer";
+
+        public const string TwitterProfilePageUrl = @"https://twitter.com/kawa0x0A";
+
+        public const string Description = "アプリケーションに関するご意見・ご要望などがありましたら\nTwitterで製作者までリプライを送っていただくか\nハッシュタグ #ホロビューワー か #HoloViewer を付けて投稿してください。\n(頂いたご要望に対応できない場合があります。ご了承ください。)";
+    }
+}
