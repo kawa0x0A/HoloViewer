12ee0fdc2589d2c21ba22e1f4a993bc75768747d
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2020-11-16 22:54:50 +0900

    Mac版の変更に合わせてWindows版の処理を変更

---
+++ HoloViewer.Windows/FeedBackInfo.cs
@@ -0,0 +1,17 @@
+namespace HoloViewer.Windows
+{
+    public class FeedBackInfo
+    {
+        public string Version { get; } = FeedBackInfoValue.Version;
+
+        public string ReleaseDate { get; } = FeedBackInfoValue.ReleaseDate;
+
+        public string SoftwareLicence { get; } = FeedBackInfoValue.SoftwareLicence;
+
+        public string RepositoryPageUrl { get; } = FeedBackInfoValue.RepositoryPageUrl;
+
+        public string TwitterProfilePageUrl { get; } = FeedBackInfoValue.TwitterProfilePageUrl;
+
+        public string FeedBackDescription { get; } = FeedBackInfoValue.FeedBackDescription;
+    }
+}
