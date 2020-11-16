37c1ff94d4727de86cf77028973a358616c8ae36
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2020-11-16 21:47:41 +0900

    フィードバック説明用のテキストを変更

--- HoloViewer/FeedBackInfo.cs
+++ HoloViewer/FeedBackInfo.cs
@@ -29,6 +29,6 @@ namespace HoloViewer
 #if __MACOS__
         [Export(nameof(FeedBackDescription))]
 #endif
-        public string FeedBackDescription { get; } = "アプリケーションに関するご意見・ご要望などがありましたら\nTwitterで製作者までリプライを送っていただくか\nハッシュタグ #ホロビューワー か #HoloViewer を付けて投稿してください。\n(頂いたご要望に対応できない場合があります。ご了承ください。)";
+        public string FeedBackDescription { get; } = "アプリケーションに関するご意見・ご要望などがありましたら\nTwitterで製作者までリプライを送っていただくか\nハッシュタグ #ホロビューワー か #HoloViewer を付けてご投稿ください。\n(頂いたご要望に対応できない場合があります。ご了承ください。)";
     }
 }
