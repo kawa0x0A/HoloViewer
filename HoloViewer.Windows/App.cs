624683bc3589396774e3a631ea18d6a3836939ca
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2020-12-22 10:18:12 +0900

    Windows版のタイトルバーの表示をクラシック形式に変更する処理を追加

--- HoloViewer.Windows/App.cs
+++ HoloViewer.Windows/App.cs
@@ -20,5 +20,29 @@ namespace HoloViewer.Windows
             BlazorHybridWindows.Init();
             LoadApplication(new App());
         }
+
+        protected override void OnActivated (EventArgs e)
+        {
+            base.OnActivated(e);
+
+            var topAppBar = Template.FindName("PART_TopAppBar", this) as Xamarin.Forms.Platform.WPF.Controls.FormsAppBar; ;
+
+            if (topAppBar != null)
+            {
+                topAppBar.Height = 0;
+            }
+
+            var commandsBar = Template.FindName("PART_CommandsBar", this) as System.Windows.Controls.Grid;
+
+            if (commandsBar != null)
+            {
+                var titleTextBlock = commandsBar.FindName("PART_System_Title") as System.Windows.Controls.TextBlock;
+
+                if (titleTextBlock != null)
+                {
+                    titleTextBlock.Text = Application.MainPage.Title;
+                }
+            }
+        }
     }
 }
