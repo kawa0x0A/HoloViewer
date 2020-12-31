a5e684b433dc2fef81348d5c89e10fa32348fd1b
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2020-12-31 13:41:59 +0900

    アプリケーション設定の定義ファイルのコミットが抜けていたので修正

---
+++ HoloViewer/ApplicationSettings.cs
@@ -0,0 +1,9 @@
+namespace HoloViewer
+{
+    public class ApplicationSettings
+    {
+        public string StartupPageUrl { get; set; } = "";
+
+        public bool IsEnableUpdateCheck { get; set; } = true;
+    }
+}
