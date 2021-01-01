147d18388735396d1301ae34112aab8151039153
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2021-01-01 14:39:11 +0900

    Mac版アプリケーションで設定ファイルを保存できない不具合を修正するために処理を変更

--- HoloViewer/ApplicationSettings.cs
+++ HoloViewer/ApplicationSettings.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Text.Json;
 using Xamarin.Forms;
@@ -13,16 +12,27 @@ namespace HoloViewer
 
         public bool IsEnableUpdateCheck { get; set; } = true;
 
+        private static string GetApplicationSettingsFilePath()
+        {
+            switch(Device.RuntimePlatform)
+            {
+                case Device.WPF: return ApplicationSettingFileName;
+                case Device.macOS: return Path.Combine($"/Users/{System.Environment.UserName}/Library/Preferences/HoloViewer/", ApplicationSettingFileName);
+            }
+
+            return "";
+        }
+
         public static bool ExistsApplicationSettingsFile()
         {
-            return File.Exists(ApplicationSettingFileName);
+            return File.Exists(GetApplicationSettingsFilePath());
         }
 
         public static ApplicationSettings Load()
         {
             var jsonString = "";
 
-            using (var streamReader = new StreamReader(ApplicationSettingFileName))
+            using (var streamReader = new StreamReader(GetApplicationSettingsFilePath()))
             {
                 jsonString = streamReader.ReadToEnd();
             }
@@ -32,9 +42,16 @@ namespace HoloViewer
 
         public static void Save(ApplicationSettings applicationSettings)
         {
+            string directoryPath = Path.GetDirectoryName(GetApplicationSettingsFilePath());
+
+            if((directoryPath != "") && (!Directory.Exists(directoryPath)))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
             var jsonString = JsonSerializer.Serialize(applicationSettings);
 
-            using (var streamWriter = new StreamWriter(ApplicationSettingFileName))
+            using (var streamWriter = new StreamWriter(GetApplicationSettingsFilePath()))
             {
                 streamWriter.Write(jsonString);
             }
